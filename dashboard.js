512bef3caef48532c64f21124587d35f7c2383ad
mahimjohn <56763704+mahimjohn@users.noreply.github.com>
2026-01-28 04:06:38 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -2,20 +2,40 @@ const auth = firebase.auth();
 
 const userPhoto = document.getElementById("userPhoto");
 const userName = document.getElementById("userName");
+
+const sidePhoto = document.getElementById("sidePhoto");
+const sideName = document.getElementById("sideName");
+
 const logoutBtn = document.getElementById("logoutBtn");
 
-auth.onAuthStateChanged(user=>{
+/* CHECK LOGIN */
+
+auth.onAuthStateChanged(user => {
 
   if(!user){
-    window.location.href="login.html";
+    window.location.href = "index.html";
   }else{
-    userPhoto.src = user.photoURL;
-    userName.innerText = user.displayName;
+
+    // Top bar
+    userPhoto.src = user.photoURL || "logos/logo.png";
+    userName.innerText = user.displayName || "User";
+
+    // Sidebar
+    sidePhoto.src = user.photoURL || "logos/logo.png";
+    sideName.innerText = user.displayName || "User";
+
   }
 
 });
 
-logoutBtn.onclick = ()=>{
-  auth.signOut();
-  window.location.href="index.html";
-};
+/* LOGOUT */
+
+logoutBtn.addEventListener("click", () => {
+
+  auth.signOut()
+  .then(() => {
+    window.location.href = "index.html";
+  })
+  .catch(err => alert(err.message));
+
+});
