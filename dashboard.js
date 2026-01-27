9e997d1ffa8a6dbf53d3551d92110056631f4fc0
mahimjohn <56763704+mahimjohn@users.noreply.github.com>
2026-01-28 05:05:10 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -6,37 +6,34 @@ const auth = firebase.auth();
 
 const userPhoto = document.getElementById("userPhoto");
 const userName = document.getElementById("userName");
-
 const sidePhoto = document.getElementById("sidePhoto");
 const sideName = document.getElementById("sideName");
-
 const logoutBtn = document.getElementById("logoutBtn");
 
-/* CHECK LOGIN */
-auth.onAuthStateChanged(user => {
+/* AUTH CHECK */
 
+auth.onAuthStateChanged(user=>{
   if(!user){
-    window.location.href = "index.html";
+    window.location.href="index.html";
   }else{
-
     userPhoto.src = user.photoURL || "logos/logo.png";
     userName.innerText = user.displayName || "User";
 
     sidePhoto.src = user.photoURL || "logos/logo.png";
     sideName.innerText = user.displayName || "User";
-
   }
-
 });
 
 /* LOGOUT */
-logoutBtn.onclick = () => {
+
+logoutBtn.onclick = ()=>{
   auth.signOut().then(()=>{
     window.location.href="index.html";
   });
 };
 
 /* SIDEBAR TOGGLE */
+
 menuBtn.onclick = ()=>{
   sidebar.classList.toggle("show");
   main.classList.toggle("shift");
