92c39428a0ddd0b76d5a909c26a9230b85987942
mahimjohn <56763704+mahimjohn@users.noreply.github.com>
2026-02-01 22:51:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,16 @@
-// Firebase core
-importScripts = undefined; // safety for browser
+// firebase.js
 
-// Your Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyB6-aF6ycLVrmLBAr-1X4fL6ZVgwF0agFc",
   authDomain: "yeshu-ke-nanhe-dost.firebaseapp.com",
   databaseURL: "https://yeshu-ke-nanhe-dost-default-rtdb.firebaseio.com",
   projectId: "yeshu-ke-nanhe-dost",
-  storageBucket: "yeshu-ke-nanhe-dost.appspot.com",
+  storageBucket: "yeshu-ke-nanhe-dost.firebasestorage.app",
   messagingSenderId: "496839768513",
   appId: "1:496839768513:web:e49e55eb2d196c8e095740"
 };
 
-// Prevent double initialization
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 console.log("Firebase Connected");
